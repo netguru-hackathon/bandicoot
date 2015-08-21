b9107e50ba90d5f47015a1db6a928ed873cdc24c
bartuz <filip.bartuzi@gmail.com>
2015-08-21 15:23:00 +0200

    Add paginate_url setter

--- lib/bandicoot/dsl.rb
+++ lib/bandicoot/dsl.rb
@@ -8,6 +8,16 @@ module Bandicoot
 
     def attributes
       @attributes ||= []
+    def scrap(options = {})
     end
+
+    def paginate_url(url, next_page_path:)
+      next_page_path = next_page
+      url = url
+    end
+
+    private
+
+    attr_accessor :next_page, :url, :origin_url, :next_page_path
   end
 end
